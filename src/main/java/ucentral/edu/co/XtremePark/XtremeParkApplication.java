9f4f1a131a61582d1872d99b91caff1271a8f0e3
JenniferLV285 <jlopezv6@ucentral.edu.co>
2025-11-02 03:25:18 -0500

    Carga inicial Estructura de trabajo

---
+++ src/main/java/ucentral/edu/co/XtremePark/XtremeParkApplication.java
@@ -0,0 +1,13 @@
+package ucentral.edu.co.XtremePark;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class XtremeParkApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(XtremeParkApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ucentral/edu/co/XtremePark/XtremeParkApplicationTests.java
@@ -0,0 +1,13 @@
+package ucentral.edu.co.XtremePark;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class XtremeParkApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
